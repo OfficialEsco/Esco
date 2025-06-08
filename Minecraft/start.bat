c8dab4ec84593bb74d1e42d99772d46954d88e52
Esco <OfficialEsco@users.noreply.github.com>
2025-06-08 16:31:44 +0200

    Cleanup unused files

--- Minecraft/start.bat
+++
@@ -1,3 +0,0 @@
-title Main Server
-java -Xmx10G -Xms10G -jar modpack.jar nogui -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing -XX:+CMSParallelRemarkEnabled -XX:MaxGCPauseMillis=50 -XX:+DisableExplicitGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -XX:+UseBiasedLocking -XX:TargetSurvivorRatio=90
-pause
